3310a74dfc861ec3df9a62ef88ee1aa032d6c166
m2m tech <tech@m2m.at>
2012-09-16 18:56:36 +0200

    assignment #2 task #4

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -9,7 +9,13 @@
 import acm.program.*;
 
 public class PythagoreanTheorem extends ConsoleProgram {
+	
 	public void run() {
-		/* You fill this in */
+		println("Enter values to compute Pythagorean theorem.");
+		int a = readInt("a: ");
+		int b = readInt("b: ");
+		double c = Math.sqrt(a * a + b * b);
+		println("c: " + c);
 	}
+	
 }
