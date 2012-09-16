d26dc9b2f0501253ac17cfef693f13fc557e304b
m2m tech <tech@m2m.at>
2012-09-16 19:47:55 +0200

    assignment #2 task #6

--- Hailstone.java
+++ Hailstone.java
@@ -5,12 +5,27 @@
  * --------------------
  * This file is the starter file for the Hailstone problem.
  */
-
 import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
+	
 	public void run() {
-		/* You fill this in */
+		int steps = 0;
+		int number = readInt("Enter a number: ");
+		while (number != 1) {
+			if (number % 2 == 1) {
+				int temp = 3 * number + 1;
+				println(number + " is odd, so I make 3n+1: " + temp);
+				number = temp;
+			} else {
+				int temp = number / 2;
+				println(number + " is even, so I take half: " + temp);
+				number = temp;			
+			}
+			steps++;
+		}
+		println("The process took " + steps + " steps to reach 1.");
 	}
+	
 }
 
