da7be15dfd8f56edb198db51b655e95aee89fee3
m2m tech <tech@m2m.at>
2012-09-16 13:35:18 +0200

    assignment #2 task #1

--- Pyramid.java
+++ Pyramid.java
@@ -9,7 +9,6 @@
  * that changing these values causes the generated display
  * to change accordingly.
  */
-
 import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
@@ -26,7 +25,36 @@ public class Pyramid extends GraphicsProgram {
 	private static final int BRICKS_IN_BASE = 14;
 	
 	public void run() {
-		/* You fill this in. */
+		double x = (getWidth() - BRICKS_IN_BASE * BRICK_WIDTH) / 2;
+		double y = getHeight() - BRICK_HEIGHT;
+		for (int row = 0; row < BRICKS_IN_BASE; row++) {
+			drawRow(x, y, BRICKS_IN_BASE - row);
+			y -= BRICK_HEIGHT;
+			x += BRICK_WIDTH / 2;
+		}
+	}
+
+	/**
+	 * draw single row
+	 * @param x      x-position of top left corner
+	 * @param y      y-position of top left corner
+	 * @param bricks number of bricks
+	 */
+	private void drawRow(double x, double y, int bricks) {
+		for (int i = 0; i < bricks; i++) {
+			drawBrick(x + i * BRICK_WIDTH, y);
+		}
+	}
+
+	/**
+	 * draw single brick
+	 * @param x x-position of top left corner
+	 * @param y y-position of top left corner
+	 */
+	private void drawBrick(double x, double y) {
+		GRect rect = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
+		rect.setColor(Color.BLACK);
+		add(rect);
 	}
 }
 
