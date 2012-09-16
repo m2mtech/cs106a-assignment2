22292e58690915a737a7dd4a5ee17f5315a113e3
m2m tech <tech@m2m.at>
2012-09-16 19:17:14 +0200

    assignment #2 task #5

--- FindRange.java
+++ FindRange.java
@@ -5,12 +5,32 @@
  * --------------------
  * This file is the starter file for the FindRange problem.
  */
-
 import acm.program.*;
 
 public class FindRange extends ConsoleProgram {
+	
+	private static final int SENTINEL_VALUE = 0;
+	
 	public void run() {
-		/* You fill this in */
+		println("This program finds the largest and smallest numbers.");
+
+		int value = readInt("? ");
+
+		if (value == SENTINEL_VALUE) {
+			println("No values have been entered.");
+		} else {
+			int smallest = value;
+			int largest = value;
+			while (true) {
+				value = readInt("? ");
+				if (value == SENTINEL_VALUE) break;
+				if (smallest > value) smallest = value;
+				else if (largest < value) largest = value;
+			}
+			println("smallest: " + smallest);
+			println("largest: " + largest);
+		}	
 	}
+	
 }
 
